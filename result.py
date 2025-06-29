2e37c317544291db35c3af6a59d4d74253bb9168
ZOHA RASHID <zoha.rashid.151@gmail.com>
2025-06-29 15:10:07 +0500

    Update result.py by zoha

--- result.py
+++ result.py
@@ -7,6 +7,7 @@ def show_result(score, total):
     result_win.geometry("300x200")
     result_win.config(bg="#e0ffe0")
 
+
     label = ttk.Label(result_win, text=f"You scored {score} out of {total}!", font=("Arial", 16))
     label.pack(pady=40)
 
